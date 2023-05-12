be4ccd6e81805209d7e3af04ff150bece9366637
S.M. Khalid Mahmud <khalidccnu@gmail.com>
2023-05-12 15:40:05 +0600

    app status detector added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { filterProducts } from "./utils/index.js";
+import AppStatus from "./components/AppStatus.jsx";
 import Root from "./routes/Root.jsx";
 import Home from "./routes/Home.jsx";
 import Wishlist from "./routes/Wishlist.jsx";
@@ -9,7 +10,11 @@ const App = () => {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Root />,
+      element: (
+        <AppStatus>
+          <Root />
+        </AppStatus>
+      ),
       children: [
         {
           path: "/",
